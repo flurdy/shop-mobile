23621f63c7803eb6b9188fa77aa8fad9f78fe7dd
Ivar Abrahamsen <ivar.abrahamsen@tieto.com>
2012-02-18 17:40:11 +0000

    migration to play-2.0-rc2

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,42 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import models._
 
 object Application extends Controller {
   
   def index = Action {
-    val shoppingList = List(new ShoppingItem("Bread","Brown,sliced"),new ShoppingItem("Milk","2l Skimmed"),new ShoppingItem("Cookie",""));
-    Ok(views.html.index(shoppingList));
+    Ok(views.html.index("Your new application is ready."))
   }
   
+}
+package controllers
+
+import play.api.data._
+import play.api.mvc._
+import models._
+import views._
+
+object Application extends Controller {
+
+  val shoppingList = List(
+    new ShoppingItem("Bread","Brown,sliced"),
+    new ShoppingItem("Milk","2l Skimmed"),
+    new ShoppingItem("Wholemeal rice"," jasminjasmin jasmin jasmin  jasminjasminjasminjasminjasminjasmin jasmin jasmin jasminjasmin jasmin"),
+    new ShoppingItem("Cookie",""))
+
+  def index = Action {
+    Ok(views.html.index(shoppingList))
+  }
+
+  val buyItemForm = Form {
+    of(
+      "itemId" -> text
+    )
+  }
+
+  def buyItem = Action {
+    Ok(views.html.index(shoppingList))
+  }
+
+
 }
