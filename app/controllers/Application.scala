cba657c2bac55eb1b0580c02462156af56ee2b91
Ivar Abrahamsen <ivar.abrahamsen@tieto.com>
2012-02-18 17:49:47 +0000

    tidied up

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,5 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-
-object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-  
-}
-package controllers
-
 import play.api.data._
 import play.api.mvc._
 import models._
