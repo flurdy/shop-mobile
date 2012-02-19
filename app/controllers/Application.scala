94a7e6ed49bd51a23bd72ccadf17932a7ded710b
Ivar Abrahamsen <ivar.abrahamsen@tieto.com>
2012-02-19 02:17:49 +0000

    form ok

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,30 +2,61 @@ package controllers
 
 import play.api.data._
 import play.api.mvc._
+import play.api.data.Forms._
 import models._
 import views._
+import play.Logger
 
 object Application extends Controller {
 
-  val shoppingList = List(
+  var shoppingList = List(
     new ShoppingItem("Bread","Brown,sliced"),
     new ShoppingItem("Milk","2l Skimmed"),
     new ShoppingItem("Wholemeal rice"," jasminjasmin jasmin jasmin  jasminjasminjasminjasminjasminjasmin jasmin jasmin jasminjasmin jasmin"),
     new ShoppingItem("Cookie",""))
 
   def index = Action {
-    Ok(views.html.index(shoppingList))
+    Ok(views.html.index(shoppingList,addItemForm))
   }
 
+  val addItemForm: Form[ShoppingItem] = Form(
+    mapping(
+      "name" -> text(minLength = 2),
+      "description" -> text
+   )(ShoppingItem.apply)(ShoppingItem.unapply)
+  )
+
+  def addItemToList = Action { implicit request =>
+    addItemForm.bindFromRequest.fold(
+      errors => {
+        Logger.warn("!!!"+errors)
+        Logger.debug("####1#"+shoppingList)
+        BadRequest(html.index(shoppingList,errors))
+      },
+      shoppingItem => {
+        Logger.debug("####0#"+shoppingItem)
+        Logger.debug("####1#"+shoppingList)
+        shoppingList ::= shoppingItem
+        Logger.debug("####2#"+shoppingList)
+        Ok(html.index(shoppingList,addItemForm))
+      }
+    )
+  }
+
+  def removeItemFromList = TODO
+
+  def putItemInBasket = TODO
+
+  /*
   val buyItemForm = Form {
-    of(
+    mapping(
       "itemId" -> text
     )
   }
 
-  def buyItem = Action {
-    Ok(views.html.index(shoppingList))
+  def buyItem(id: Long) = Action {
+    ShoppingItem.findById(id)
   }
-
+  */
 
 }
--- app/models/ShoppingItem.scala
+++ app/models/ShoppingItem.scala
@@ -5,5 +5,6 @@ case class ShoppingItem(name: String, description: String)
 
 object ShoppingItem {
 
+  def create(name: String) {}
 
 }
