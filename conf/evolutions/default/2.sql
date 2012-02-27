69110ffcdf4f31a8763a2ff298cbbbd3e22a6d0b
Ivar Abrahamsen <ivar.abrahamsen@tieto.com>
2012-02-27 19:04:40 +0000

    reduced dimensions

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,16 @@
+# --- First database schema
+ 
+# --- !Ups
+
+INSERT INTO shoppingitem (name,description,ispurchased)
+            values ('Tea','Red label',false);
+INSERT INTO shoppingitem (name,description,ispurchased)
+            values ('Milk','2l semi skimmed',false);
+INSERT INTO shoppingitem (name,description,ispurchased)
+            values ('Bread','Wholemeal. Medium sliced',false);
+INSERT INTO shoppingitem (name,description,ispurchased)
+            values ('Bananas','bunch of 3',false);
+
+# --- !Downs
+ 
+delete from shoppingitem;
