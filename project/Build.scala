5dd3ac216b51e819762dcf168fa2e0faa4dafa14
ivar <ivar@flurdy.com>
2013-01-26 23:51:20 +0000

    Send grid set up

--- project/Build.scala
+++ project/Build.scala
@@ -5,11 +5,12 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "shopgrid"
-    val appVersion      = "1.1-SNAPSHOT"
+    val appVersion      = "1.2-SNAPSHOT"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
+      "com.typesafe" %% "play-plugins-mailer" % "2.0.4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
