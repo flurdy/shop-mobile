9007b71db0c12361d868a0bfa00cb0e02ee28dac
ivar.abrahamsen <ivar.abrahamsen@mindcandy.com>
2013-07-19 08:57:27 +0100

    Play 2.1 Migration: Modified files.

--- project/Build.scala
+++ project/Build.scala
@@ -1,20 +1,23 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
     val appName         = "shopgrid"
-    val appVersion      = "1.2-SNAPSHOT"
+    val appVersion      = "2.0-SNAPSHOT"
 
     val appDependencies = Seq(
       // Add your project dependencies here,
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
-      "com.typesafe" %% "play-plugins-mailer" % "2.0.4"
+      "com.typesafe" %% "play-plugins-mailer" % "2.0.4",
+      jdbc,
+      anorm
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      // Add your own project settings here
     )
 
 }
