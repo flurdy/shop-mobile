6cb01c54232c43cfabcc6d7f29af12e114ef9525
Ivar <ivar@flurdy.com>
2013-07-19 09:15:24 +0100

    Play 2.1 Migration continued

--- project/Build.scala
+++ project/Build.scala
@@ -11,6 +11,9 @@ object ApplicationBuild extends Build {
       // Add your project dependencies here,
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
       "com.typesafe" %% "play-plugins-mailer" % "2.0.4",
+      "org.webjars" % "webjars-play" % "2.1.0",
+      "org.webjars" % "jquery" % "1.9.1",
+      "org.webjars" % "bootstrap" % "2.3.0",
       jdbc,
       anorm
     )
