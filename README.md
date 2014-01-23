6003d453cba1f1a83629ea1ac2898aa15015a70a
Ivar <ivar@flurdy.com>
2014-01-23 18:18:30 +0000

    Removed warnings. and commented out global 404 and 500 handlers

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
+SHOPgrid
+====
+
 A simple shopping list app.
+
 Designed to be usable on desktop and mobile.
-Uses Play 2.0 and Scala.
+Uses Play 2 and Scala.
 
 Task board: http://agilezen.com/project/33567/board
 
 Demo: http://shop.flurdy.com/
+
+Source at https://github.com/flurdy/shoppinglist
+
+License: MIT
--- app/Global.scala
+++ app/Global.scala
@@ -4,20 +4,20 @@ import play.api.mvc.Results._
 
 object Global extends GlobalSettings {
 
-  override def onHandlerNotFound(request: RequestHeader) = {
-    if (request.path != "/favicon.ico") // && Logger.isInfoEnabled)
-      Logger.info("Not found: " + request.path)
-    NotFound(views.html.errors.notfound(request.path))
-  }
+  // override def onHandlerNotFound(request: RequestHeader) = {
+    // if (request.path != "/favicon.ico") // && Logger.isInfoEnabled)
+      // Logger.info("Not found: " + request.path)
+    // NotFound(views.html.errors.notfound(request.path))
+  // }
 
   //override def onBadRequest(request: RequestHeader, error: String) = {
   //  BadRequest("Bad Request: " + error)
   //}
 
-  override def onError(request: RequestHeader, ex: Throwable) = {
-    Logger.error("Server error: " + request.path, ex)
-    InternalServerError(views.html.errors.servererror(ex))
-  }
+  // override def onError(request: RequestHeader, ex: Throwable) = {
+    // Logger.error("Server error: " + request.path, ex)
+    // InternalServerError(views.html.errors.servererror(ex))
+  // }
 
 
 }
